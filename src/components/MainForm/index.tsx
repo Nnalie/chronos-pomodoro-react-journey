2ec6991ff19c1864b32ed69e9569c7a96a4c7ae7
leonardo Carvalho <leonardoelian.c.a@gmail.com>
2025-11-24 17:47:23 -0400

    feat: Função de conversão de segundos em minutos para o contador

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,13 +3,54 @@ import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 import { useRef } from "react";
+import type { TaskModel } from "../../models/taskModel";
+import { useTaskContext } from "../../contexts/TaskContext/UseTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 
 export function MainForm() {
+  const { state, setState } = useTaskContext();
   // const [taskName, setTaskName] = useState("");
   const taskNameInput = useRef<HTMLInputElement>(null);
 
+  const nextCycle = getNextCycle(state.currenteCycle);
+  const nextCycleType = getNextCycleType(state.currenteCycle);
+
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert("Digite o nome !");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
 
   return (
@@ -23,9 +64,10 @@ export function MainForm() {
           /**
            * Utilizar 'useState' + 'onChange' quando quiser pegar o valor do input
            * em tempo real, como em uma validação de cpf em tempo real, form controlada
+           * 
+            // value={taskName}
+            // onChange={(e) => setTaskName(e.target.value)}
            */
-          // value={taskName}
-          // onChange={(e) => setTaskName(e.target.value)}
           /**
            * Utilizar 'ref' para quando não interessa o que o usuário esteja digitando, pois
            * o input será somente validado após o envio dele.
---
+++ src/utils/formatSecondsToMinutes.ts
@@ -0,0 +1,12 @@
+export function formatSecondsToMinutes(seconds: number) {
+  /* Captura os minutos e formata usando o padStart que caso não tenha um
+  número com duas casas ele adiciona o '0' na frente.
+  */
+  const minutes = String(Math.floor(seconds / 60)).padStart(2, "0");
+
+  /* Captura os segundos restantes e formata usando o padStart que caso não tenha um
+  número com duas casas ele adiciona o '0' na frente.
+  */
+  const secondsMod = String(Math.floor(seconds % 60)).padStart(2, "0");
+  return `${minutes}:${secondsMod}}`;
+}
